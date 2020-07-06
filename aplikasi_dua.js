084e973dd0bfd2f2d23dab917ca79ad4430eda25
Herley Shaori <herley.shaori@ui.ac.id>
2020-07-06 08:47:31 +0700

    belajar watchdog

--- aplikasi_dua.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-    //connect to the socket server.
-    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
-    var numbers_received = [];
-    $('#log').html('Diganti, kalau direfresh ya terganti.');
-});
